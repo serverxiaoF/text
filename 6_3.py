e8ce69110a788f008c0829327429d598a7068829
server12XF <64524567+serverxiaoF@users.noreply.github.com>
2020-08-19 09:38:19 +0800

    6_3python练习
    我的python练习

---
+++ 6_3.py
@@ -0,0 +1,18 @@
+people={
+    'first_name':'Fu',
+    'last_name':'Junming',
+    'age':'18',
+    'city':'Huizhou',
+	}
+print(people['first_name'])
+print(people)
+#6_2
+print("\nThe next progress")
+love_number={
+    'Lihua':'12',
+    'Sarah':'23',
+    'Mike':'29',
+    'John':'78',
+    'Amy':'101',
+}
+print(love_number['Mike']+" is Mike's love")
